39d255ab4ede49a0cfeaa7bb91f750a8fb69428d
cmlee <bal.dongdongdong@gmail.com>
2025-01-26 15:55:04 +0900

    :sparkles: Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import toast from "react-simple-toasts";
+import toast, { toastConfig } from "react-simple-toasts";
 import "react-simple-toasts/dist/style.css";
+import "react-simple-toasts/dist/theme/dark.css";
+import SlotCounter from "react-slot-counter";
 import "./App.css";
+
+toastConfig({
+  theme: "dark",
+});
 
 function App() {
   return (
-    <div>
-      <button type="button" onClick={() => toast("Hello World")}>
-        show toast
+    <div className="container">
+      <button
+        type="button"
+        className="button"
+        onClick={() => toast("Hello World")}
+      >
+        show toastx
       </button>
+
+      <div className="slot-container">
+        <SlotCounter value={41223} />
+      </div>
     </div>
   );
 }
